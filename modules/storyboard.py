ddde32e3775722b16678024e02fda6fdb60c07d6
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-10-31 12:35:51 -0400

    cleanup zombie code

--- modules/storyboard.py
+++ modules/storyboard.py
@@ -1,17 +1,12 @@
 import modules.scripts
-from modules.processing import StableDiffusionProcessing, Processed, StableDiffusionProcessingTxt2Img, StableDiffusionProcessingImg2Img, process_images
+from modules.processing import StableDiffusionProcessing, Processed, StableDiffusionProcessingTxt2Img, \
+    StableDiffusionProcessingImg2Img, process_images
 from modules.shared import opts, cmd_opts
 import modules.shared as shared
-import modules.processing as processing
-#from modules.ui import plaintext_to_html
 from modules.story_squad import CallArgsAsData
-#from ui import plaintext_to_html
-#from modules.story_squad import CallArgsAsData
 
 
-#def storyboard(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2: str, steps: int, sampler_index: int, restore_faces: bool, tiling: bool, n_iter: int, batch_size: int, cfg_scale: float, seed: int, subseed: int, subseed_strength: float, seed_resize_from_h: int, seed_resize_from_w: int, seed_enable_extras: bool, height: int, width: int, enable_hr: bool, scale_latent: bool, denoising_strength: float, *args):
-
-def storyboard(call_args_data:CallArgsAsData, *args):
+def storyboard(call_args_data: CallArgsAsData, *args):
     from modules.ui import plaintext_to_html
     print("Processing...")
 
@@ -59,30 +54,4 @@ def storyboard(call_args_data:CallArgsAsData, *args):
     if opts.do_not_show_images:
         processed.images = []
 
-    return processed.images, generation_info_js, plaintext_to_html(processed.info)
-
-    p.sd_model= shared.sd_model
-    print("Processing...")
-    if cmd_opts.enable_console_prompts:
-        print(f"\nstoryboard: {call_args_data.prompt}", file=shared.progress_print_out)
-
-    # check if args[0] is a tuple
-    if isinstance(args[0], tuple):
-        args = args[0]
-    processed = modules.scripts.scripts_txt2img.run(p, *args)
-
-    if processed is None:
-        processed = process_images(p)
-
-    shared.total_tqdm.clear()
-
-    generation_info_js = processed.js()
-    if opts.samples_log_stdout:
-        print(generation_info_js)
-
-    if opts.do_not_show_images:
-        processed.images = []
-
-    #return processed.images,plaintext_to_html(processed.info)
-    return processed.images,processed.seed, generation_info_js, plaintext_to_html(processed.info)
-
+    return processed.images, processed.seed, generation_info_js, plaintext_to_html(processed.info)
